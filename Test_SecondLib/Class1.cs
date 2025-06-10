68a3b372a39fa426113c1b987fbd271add72554c
Hany Fayez <hany.fayez@sys-datgroup.com>
2025-06-10 19:41:12 +0200

    Add Lib2

---
+++ Test_SecondLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_SecondLib
+{
+    public class Test_SecondLib
+    {
+        public static void Ciao()
+        {
+            Console.WriteLine("Ciao from HF_TestLib2!");
+        }
+    }
+}
